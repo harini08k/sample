7f583bde4006e62c7ff81137464fe729d6e5e7af
Harini Karthikeyan <harini93331@gmail.com>
2025-10-21 00:24:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,8 +19,7 @@ app.post("/summarize", async (req, res) => {
     
     // Use axios with the custom HTTPS agent
     const response = await axios.post(
-      "https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generate
-",
+      "https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generate",
       { prompt: { text: text } },
       {
         headers: { Authorization: `Bearer ${process.env.GEMINI_API_KEY}` },
@@ -36,6 +35,7 @@ app.post("/summarize", async (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
 
